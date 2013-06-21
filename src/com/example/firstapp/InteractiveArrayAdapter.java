955efdfb7c60eb1de207218fc5492726b72cd9d5
nikunjy <nikunjyadav89@gmail.com>
2013-06-20 23:13:05 -0700

    Text to speech, fragments

--- src/com/example/firstapp/InteractiveArrayAdapter.java
+++ src/com/example/firstapp/InteractiveArrayAdapter.java
@@ -1,5 +1,6 @@
 package com.example.firstapp;
 
+import java.io.InputStream;
 import java.util.List;
 
 import android.app.Activity;
@@ -8,29 +9,46 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import com.example.datatypes.Record;
+import com.gtranslate.Audio;
+import com.gtranslate.Language;
 
 
 public class InteractiveArrayAdapter extends ArrayAdapter<Record> {
 
 	private final List<Record> list;
 	private final Activity context;
-
+	private LoaderActivity activity;
+	String timef(double d) {
+		double time = d;
+		String amorpm="AM";
+		int hour= (int)time/60; 
+		if(time/60>=12){
+			hour-=12;
+			amorpm="PM";
+		}
+		int mins = (int)time%60;
+		return hour+":"+mins+" "+amorpm;
+		
+	}
 	public InteractiveArrayAdapter(Activity context, List<Record> list) {
 		super(context, android.R.layout.simple_list_item_1, list);
 		this.context = context;
+		this.activity = (LoaderActivity)context;
 		this.list = list;
 	}
 
 	static class ViewHolder {
 		protected TextView text;
 		protected CheckBox checkbox;
+		protected Button button;
 		//protected TextView text2;
-		
+
 	}
 
 	@Override
@@ -41,7 +59,7 @@ public class InteractiveArrayAdapter extends ArrayAdapter<Record> {
 			view = inflator.inflate(R.layout.rowbuttonlayout, null);
 			final ViewHolder viewHolder = new ViewHolder();
 			viewHolder.text = (TextView) view.findViewById(R.id.label);
-			
+			viewHolder.button=(Button)view.findViewById(R.id.viewmap);
 			viewHolder.checkbox = (CheckBox) view.findViewById(R.id.check);
 			viewHolder.checkbox
 			.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -51,9 +69,10 @@ public class InteractiveArrayAdapter extends ArrayAdapter<Record> {
 						boolean isChecked) {
 					Record element = (Record) viewHolder.checkbox
 							.getTag();
+					viewHolder.button.setVisibility(View.VISIBLE);
+					activity.clickUser(element);
 					Log.i("User selected",element.getUserId());
-					//element.setSelected(buttonView.isChecked());
-
+					
 				}
 			});
 			view.setTag(viewHolder);
@@ -63,9 +82,8 @@ public class InteractiveArrayAdapter extends ArrayAdapter<Record> {
 			((ViewHolder) view.getTag()).checkbox.setTag(list.get(position));
 		}
 		ViewHolder holder = (ViewHolder) view.getTag();
-		holder.text.setText(list.get(position).getUserId());
-		//holder.text2.setText(list.get(position).
-			//	getCurrentLat()+ "  "+list.get(position).getCurrentLong());
+		double time = list.get(position).getTime();
+		holder.text.setText(list.get(position).getUserId()+"\n"+timef(time));
 		holder.checkbox.setChecked(false);
 		return view;
 	}
--- src/com/example/firstapp/LoaderActivity.java
+++ src/com/example/firstapp/LoaderActivity.java
@@ -1,60 +1,57 @@
 package com.example.firstapp;
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 
-import org.apache.http.client.ClientProtocolException;
-import org.json.JSONArray;
 import org.json.JSONObject;
+
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.connections.ConnectionsManager;
 import com.example.connections.HitClient;
 import com.example.datatypes.Record;
 import com.google.gson.Gson;
 
-import android.app.Activity;
-import android.app.ListActivity;
-import android.app.ProgressDialog;
-import android.content.Context;
-import android.content.Intent;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.TextView;
-
-public class LoaderActivity extends ListActivity implements OnClickListener {
+public class LoaderActivity extends ListActivity implements OnClickListener, OnInitListener {
 	Button btn;
 	HitClient client = new HitClient();
 	String uri=ConnectionsManager.db_url+"users.json";
-
+	ProgressDialog dialog;
 	TextView heading;
+	TextToSpeech tts;
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle icicle) {
 		super.onCreate(icicle);
 		setContentView(R.layout.activity_loader);
+		tts = new TextToSpeech(this,this);
 	}
+	
+	@Override
+	public void onDestroy() {
 
+		if (tts != null) {
+			tts.stop();
+			tts.shutdown();
+		}
+		super.onDestroy();
+	}
+	
 
 	public void onClick(View view){
-		//detect the view that was "clicked"
-		switch(view.getId())
-		{
-		case R.id.button2:
-			new LongOperation(this,null).execute("");
-			break;
-
-		}
 
 	}
 	public List<String> getPotentialDrivers()
@@ -80,24 +77,23 @@ public class LoaderActivity extends ListActivity implements OnClickListener {
 		return output;
 	}
 
+	public void clickUser(Record record) { 
+		Log.i("came","here");
+		tts.speak(record.getUserId()+" has been sent a bid", TextToSpeech.QUEUE_FLUSH, null);
+
+	}
 	public void submit(View view) {
-
-		String[] messages =new String[2]; 
-		AsyncTask task = new LongOperation(this,messages).execute();
-
+		new LongOperation(this).execute();
 	}
 
 	private class LongOperation extends AsyncTask<String, Void, String> {
 		ListActivity activity;
 		private Context context;
-		private ProgressDialog dialog;
 		List<String> messages;
-		String[] msgs;
-		public LongOperation(ListActivity activity,String[] msgs)  {
+		public LongOperation(ListActivity activity)  {
 			this.activity = activity;
 			context = activity;
-			dialog = new ProgressDialog(context);
-			this.msgs=msgs;
+
 			messages = new ArrayList<String>(); 
 
 		}
@@ -113,7 +109,6 @@ public class LoaderActivity extends ListActivity implements OnClickListener {
 			Gson gson = new Gson();
 			for (String msg : messages){
 				Record rec = gson.fromJson(msg,Record.class);
-				Log.i("User",rec.getUserId());
 				titles.add(rec); 
 			}
 
@@ -129,13 +124,32 @@ public class LoaderActivity extends ListActivity implements OnClickListener {
 
 		@Override
 		protected void onPreExecute() {
-			dialog.setTitle("Loading");
-			dialog.setMessage("Wait while loading...");
+			dialog = ProgressDialog.show(activity, "Working..", "Downloading Data...", true, false);
+
 		}
 
 		@Override
 		protected void onProgressUpdate(Void... values) {
 		}
+	}
+
+	@Override
+	public void onInit(int status) {
+		if (status == TextToSpeech.SUCCESS) {
+
+			int result = tts.setLanguage(Locale.US);
+
+			if (result == TextToSpeech.LANG_MISSING_DATA
+					|| result == TextToSpeech.LANG_NOT_SUPPORTED) {
+				Log.e("TTS", "This Language is not supported");
+			} else {
+
+			}
+
+		} else {
+			Log.e("TTS", "Initilization Failed!");
+		}
+
 	}
 
 }
